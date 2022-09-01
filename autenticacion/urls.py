ee9c28c9364239bddb6107effab2785f3af26016
Nahuel Villarreal <nahuelv77@gmail.com>
2022-09-01 01:58:04 -0300

    funcionalidad change password

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     
     path('', views.logear, name="login"),
-    path('cerrar_sesion', views.cerrar_sesion, name='cerrar_sesion')
+    path('cerrar_sesion', views.cerrar_sesion, name='cerrar_sesion'),
+    path('change_pass', views.change_pass, name='change_pass'),
+
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from usuarios.models import Personal
 
 # Create your views here.
 
@@ -27,7 +28,24 @@ def logear(request):
     form = AuthenticationForm()
     return render(request, 'login/login.html', {'form':form})
 
-
 def cerrar_sesion(request):
     logout(request)
     return redirect('login')
+
+def change_pass(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    elif request.method == "POST":
+        cont = request.POST["newpass"]
+        newcont = request.POST["newpassconfirm"]
+        if cont == newcont:
+            u = Personal.objects.get(matricula=request.user.matricula)
+            u.set_password(cont)
+            u.save()
+            messages.success(request, "Contraseña cambiada correctamente")
+            usuario = authenticate(matricula = request.user.matricula, password=cont)
+            login(request, usuario)
+            return redirect('perfil')
+        else:
+            messages.error(request, "Contraseñas no coinciden")
+    return render(request, 'login/change-pass.html')
