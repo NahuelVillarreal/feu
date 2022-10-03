865af28b6bceb5dee66e3c7de3e4d79bcdd332d3
Nahuel Villarreal <nahuelv77@gmail.com>
2022-10-03 02:58:02 -0300

    Hecho layout cuarteleros

--- cuarteleros/urls.py
+++ cuarteleros/urls.py
@@ -3,6 +3,12 @@ from cuarteleros import views
 
 
 urlpatterns = [
-    path('', views.standby, name="standby"),
+    path('', views.logear2, name="logincuarteleros"),
+    path('standby', views.standby, name="standby"),
+    path('agnadir-servicio', views.agnadir_servicio, name="agnadir-servicio"),
+    path('agnadir-asistencia', views.agnadir_asistencia, name="agnadir-asistencia"),
+    path('servicios', views.servicios, name="servicios"),
+    path('asistencias', views.asistencias, name="asistencias"),
+    path('personal', views.personal, name="personal"),
 ]
 
--- cuarteleros/views.py
+++ cuarteleros/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from usuarios.models import Personal
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+def logear2(request):
+    if request.user.is_authenticated:
+        if request.user.groups.filter(name__in=['Cuarteleros']):
+            return redirect('standby')
+        else:
+            return redirect('inicio')
+    elif request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get('username')
+            contra = form.cleaned_data.get('password')
+            usuario = authenticate(matricula = nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('standby')
+        else:
+            messages.error(request, 'Información incorrecta.')
+            return redirect('logincuarteleros')
+
+    form = AuthenticationForm()
+    return render(request, 'cuarteleros/logincuarteleros.html', {'form':form})
+
 def standby(request):
-    return render(request, "standby.html")
+    if request.user.groups.filter(name__in=['Cuarteleros']):
+        return render(request, "cuarteleros/standby.html")
+    else:
+        logout(request)
+        messages.error(request, 'No eres cuartelero.')
+        return redirect('logincuarteleros')
+
+def agnadir_servicio(request):
+    return render(request, 'cuarteleros/agnadir-servicio.html')
+
+def agnadir_asistencia(request):
+    return render(request, 'cuarteleros/agnadir-asistencia.html')
+
+def servicios(request):
+    return render(request, 'cuarteleros/servicios.html')
+
+def asistencias(request):
+    return render(request, 'cuarteleros/asistencias.html')
+
+def personal(request):
+    return render(request, 'cuarteleros/personal-cuartelero.html')
+
